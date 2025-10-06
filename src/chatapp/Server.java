ed37d16be9147eb3c09bf5948146c8a80c66fe1f
Rabindra-1 <sahrabindra394@gmail.com>
2025-10-06 11:29:00 +0545

    Chatapp

---
+++ src/chatapp/Server.java
@@ -0,0 +1,41 @@
+package chatapp;
+
+import java.awt.Color;
+import javax.swing.*;
+
+public class Server extends JFrame {
+
+    public Server() {
+        setLayout(null);
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setLayout(null);
+        p1.setBounds(0, 0, 450, 90);
+        add(p1);
+
+
+        
+        setSize(450, 700);
+        setLocation(200, 50);
+        getContentPane().setBackground(Color.WHITE);
+        
+        
+        
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/Add.ico"));
+        JLabel back = new JLabel(i1);
+        back.setBounds(0, 0, 50, 50);
+        p1.add(back);
+        
+        
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        new Server();
+    }
+
+}
